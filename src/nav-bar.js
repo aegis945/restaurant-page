b251299369a815446a1acf4fe3f91abf55543860
aegis945 <artiom.lekincev@gmail.com>
2024-01-19 23:21:49 +0200

    fix gap on mobile

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -4,7 +4,7 @@ const createNavBar = () => {
     const navBar = document.createElement('nav');
     navBar.classList.add('flex', 'justify-center');
     navBar.innerHTML = `
-        <ul class="flex justify-center gap-28 bg-yellow-100 bg-opacity-70 w-screen py-1.5">
+        <ul class="flex justify-center gap-20 lg:gap-28 bg-yellow-100 bg-opacity-70 w-screen py-1.5">
             <li id="home" class="text-black hover:cursor-pointer text-lg">Home</li>
             <li id="menu" class="text-black hover:cursor-pointer text-lg">Menu</li>
             <li id="contact" class="text-black hover:cursor-pointer text-lg">Contact</li>
